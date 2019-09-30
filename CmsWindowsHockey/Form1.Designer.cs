e49e749553027e1fe5e48d72551c43cda58d818a
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-09-30 14:55:01 +0200

    changes

--- CmsWindowsHockey/Form1.Designer.cs
+++ CmsWindowsHockey/Form1.Designer.cs
@@ -29,7 +29,15 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.namnFilter = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labGridView = new System.Windows.Forms.DataGridView();
+            this.lab1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.labGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -39,26 +47,106 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1203, 428);
+            this.dataGridView1.Size = new System.Drawing.Size(816, 428);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(106, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(239, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Season";
+            // 
+            // namnFilter
+            // 
+            this.namnFilter.Location = new System.Drawing.Point(593, 34);
+            this.namnFilter.Name = "namnFilter";
+            this.namnFilter.Size = new System.Drawing.Size(294, 22);
+            this.namnFilter.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(463, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Namn innehåller";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1004, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Visa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // labGridView
+            // 
+            this.labGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.labGridView.Location = new System.Drawing.Point(848, 119);
+            this.labGridView.Name = "labGridView";
+            this.labGridView.RowHeadersWidth = 51;
+            this.labGridView.RowTemplate.Height = 24;
+            this.labGridView.Size = new System.Drawing.Size(425, 417);
+            this.labGridView.TabIndex = 6;
+            // 
+            // lab1
+            // 
+            this.lab1.Location = new System.Drawing.Point(1295, 119);
+            this.lab1.Name = "lab1";
+            this.lab1.Size = new System.Drawing.Size(75, 23);
+            this.lab1.TabIndex = 7;
+            this.lab1.Text = "Lab 1";
+            this.lab1.UseVisualStyleBackColor = true;
+            this.lab1.Click += new System.EventHandler(this.Lab1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1227, 559);
+            this.ClientSize = new System.Drawing.Size(1393, 559);
+            this.Controls.Add(this.lab1);
+            this.Controls.Add(this.labGridView);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.namnFilter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.labGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox namnFilter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView labGridView;
+        private System.Windows.Forms.Button lab1;
     }
 }
 
--- CmsWindowsHockey/Form1.cs
+++ CmsWindowsHockey/Form1.cs
@@ -14,9 +14,23 @@ namespace CmsWindowsHockey
     public partial class Form1 : Form
     {
         List<HockeyTeam> teamsList = new List<HockeyTeam>();
+        string[] bilar = { "Volvo", "BMW", "Audi", "Skoda",
+"Toyota", "Ford", "Mercedes","Seat", "Honda",
+"Volkswagen","Opel", "Mazda","Suzuki" };
         public Form1()
         {
             InitializeComponent();
+        }
+
+        class HockeyTeamView
+        {
+            public string Name { get; set; }
+            public int GP { get; set; }
+            public int WonFullTime { get; set; }
+            public int WonOvertime { get; set; }
+            public int LostOvertime { get; set; }
+            public int LostFulltime { get; set; }
+            public int Points { get; set; }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -25,6 +39,12 @@ namespace CmsWindowsHockey
             teamsList.AddRange(ReadFromFile("Elitserien", "2018/2019", "..\\..\\hockey2.txt"));
 
 
+            List<HockeyTeam> list = teamsList.Where(r => r.Goals > 10).ToList();
+            int antal = teamsList.Count(r=>r.Goals > 10);
+
+            var uniqueList = teamsList.Select(t => t.Season).Distinct();
+            foreach (var season in uniqueList)
+                comboBox1.Items.Add(season);
         }
 
         private static IEnumerable<HockeyTeam> ReadFromFile(string serie, string season, string file)
@@ -48,5 +68,47 @@ namespace CmsWindowsHockey
 
         }
 
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string selectedSeason = comboBox1.SelectedItem.ToString();
+            string nameToSearchFor = namnFilter.Text;
+
+
+
+            dataGridView1.DataSource = teamsList
+                                            .Where(team => team.Season == selectedSeason && 
+                                                           team.Name.Contains(nameToSearchFor)     )
+                                            .OrderByDescending(team => team.Points)
+                                            .Select(team=>new HockeyTeamView
+                                            {
+                                                    Name = team.Name,
+                                                    GP = team.GamesPlayed,
+                                                    LostFulltime = team.ZeroP,
+                                                    LostOvertime = team.OneP,
+                                                    WonFullTime = team.ThreeP,
+                                                    WonOvertime = team.TwoP,
+                                                    Points = team.Points
+                                            })
+                                            .ToList();
+
+        }
+        class View
+        {
+            public View(string n)
+            {
+                name = n;
+            }
+            public string name { get; set; }
+        }
+
+        private void Lab1_Click(object sender, EventArgs e)
+        {
+            labGridView.DataSource = bilar.Where(bil => bil.StartsWith("V"))
+                .Select(r=>new View(r)).ToList();
+        }
     }
 }
