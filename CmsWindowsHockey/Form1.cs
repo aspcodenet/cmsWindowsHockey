2ff8e50101f81dcc9569bc021e30b37829c44cf9
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-09-30 12:05:56 +0200

    Add project files.

---
+++ CmsWindowsHockey/Form1.cs
@@ -0,0 +1,52 @@
+﻿using CmsWindowsHockey.Hockey;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CmsWindowsHockey
+{
+    public partial class Form1 : Form
+    {
+        List<HockeyTeam> teamsList = new List<HockeyTeam>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            teamsList.AddRange(ReadFromFile("Elitserien", "2019/2020", "..\\..\\hockey.txt"));
+            teamsList.AddRange(ReadFromFile("Elitserien", "2018/2019", "..\\..\\hockey2.txt"));
+
+
+        }
+
+        private static IEnumerable<HockeyTeam> ReadFromFile(string serie, string season, string file)
+        {
+            foreach (var line in System.IO.File.ReadAllLines(file))
+            {
+                var t = new HockeyTeam();
+                var parts = line.Split('\t');
+                t.Serie = serie;
+                t.Season = season;
+                t.Name = parts[1];
+                t.GamesPlayed = Convert.ToInt32(parts[2]);
+                t.ThreeP = Convert.ToInt32(parts[3]);
+                t.TwoP = Convert.ToInt32(parts[4]);
+                t.OneP = Convert.ToInt32(parts[5]);
+                t.ZeroP = Convert.ToInt32(parts[6]);
+                t.Goals = Convert.ToInt32(parts[7]);
+                t.GoalsInslappta = Convert.ToInt32(parts[8]);
+                yield return t;
+            }
+
+        }
+
+    }
+}
---
+++ CmsWindowsHockey/Hockey/HockeyTeam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmsWindowsHockey.Hockey
+{
+    class HockeyTeam
+    {
+        public string Serie { get; set; }
+        public string Name { get; set; }
+        public int GamesPlayed { get; set; }
+        public int ThreeP { get; set; }
+        public int TwoP { get; set; }
+        public int OneP { get; set; }
+        public int ZeroP { get; set; }
+        public int Goals { get; set; }
+        public int GoalsInslappta { get; set; }
+
+        public string Coach { get; set; }
+        public string Season { get; set; }
+
+        public int Points { get { return ThreeP * 3 + TwoP * 2 + OneP; } }
+    }
+}
